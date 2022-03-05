23476fd35f7bffc8791956e18ea157820f44f445
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-03-05 14:08:24 +0300

    Finished Authentication for Signup Activity and started  on the Authentication for the Login Activity

--- app/src/main/java/com/moringaschool/moversapp/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/moversapp/ui/LoginActivity.java
@@ -7,20 +7,30 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.moringaschool.moversapp.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    @BindView(R.id.email) TextInputLayout memail;
-    @BindView(R.id.password) TextInputLayout mpassword;
-    @BindView(R.id.login) Button mlogin;
-    @BindView(R.id.createaccountclick) TextView mcreate;
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    @BindView(R.id.email) TextInputLayout email;
+    @BindView(R.id.password) TextInputLayout password;
+    @BindView(R.id.login) Button mLoginButton;
+    @BindView(R.id.createaccountclick) TextView mCreateAccountClickTextView;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private String mEmail;
+    private String mPassword;
 
 
     @Override
@@ -28,18 +38,46 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        mlogin.setOnClickListener(this);
-        mcreate.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+        mCreateAccountClickTextView.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags()
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
 
     @Override
     public void onClick(View view) {
-        if(view == mlogin){
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
+        if(view == mLoginButton) {
 
-        }else if (view == mcreate){
+        }
+        if (view == mCreateAccountClickTextView) {
             Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
             startActivity(intent);
             finish();
--- app/src/main/java/com/moringaschool/moversapp/ui/SignupActivity.java
+++ app/src/main/java/com/moringaschool/moversapp/ui/SignupActivity.java
@@ -3,26 +3,36 @@ package com.moringaschool.moversapp.ui;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.moringaschool.moversapp.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
-    @BindView(R.id.name) TextInputLayout name;
-    @BindView(R.id.email) TextInputLayout email;
-    @BindView(R.id.password) TextInputLayout password;
-    @BindView(R.id.confirmpassword) TextInputLayout confirmPassword;
+    private static final String TAG = SignupActivity.class.getSimpleName();
+    @BindView(R.id.name) TextInputLayout nameTextInputLayout;
+    @BindView(R.id.email) TextInputLayout emailTextInputLayout;
+    @BindView(R.id.password) TextInputLayout passwordTextInputLayout;
+    @BindView(R.id.confirmpassword) TextInputLayout confirmPasswordTextInputLayout;
     @BindView(R.id.signup) Button signUpButton;
     @BindView(R.id.loginClick) TextView loginClickTextView;
 
@@ -41,6 +51,36 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         ButterKnife.bind(this);
         signUpButton.setOnClickListener(this);
         loginClickTextView.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
 
     @Override
@@ -52,11 +92,103 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
             finish();
         }
         if (view == signUpButton){
-            Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            finish();
+            showProgressBar();
+            createNewUser();
         }
 
     }
+
+    private void createNewUser() {
+        mName = nameTextInputLayout.getEditText().getText().toString().trim();
+        mEmail = emailTextInputLayout.getEditText().getText().toString().trim();
+        mPassword = passwordTextInputLayout.getEditText().getText().toString().trim();
+        mConfirmPassword = confirmPasswordTextInputLayout.getEditText().getText().toString().trim();
+
+        if (confirmName() == false) {
+            return;
+        }
+        if (confirmEmail() == false) {
+            return;
+        }
+        if (confirmPassword() == false) {
+            return;
+        }
+        if (passwordsMatch() == false) {
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(mEmail, mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.e(TAG, "Authentication Successful");
+                    updateFirebaseUserProfile(task.getResult().getUser());
+                } else {
+                    Toast.makeText(SignupActivity.this, "Signup Failed!", Toast.LENGTH_LONG).show();
+                    Log.e(TAG, "Authentication Failed: ", task.getException());
+                }
+            }
+        });
+
     }
+
+    private void updateFirebaseUserProfile(FirebaseUser user) {
+
+        UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+                                                    .setDisplayName(mName)
+                                                    .build();
+
+        user.updateProfile(request).addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                hideProgressBar();
+                if(task.isSuccessful()) {
+                    Log.e(TAG, user.getDisplayName());
+                } else {
+                    Log.e(TAG, "Error: ", task.getException());
+                }
+            }
+        });
+    }
+
+    private boolean passwordsMatch() {
+        if (!mPassword.equals(mConfirmPassword)) {
+            passwordTextInputLayout.getEditText().setError("Passwords must match!");
+            confirmPasswordTextInputLayout.getEditText().setError("Passwords must match!");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean confirmPassword() {
+        if (mPassword.length() < 6 || mConfirmPassword.length() < 6) {
+            passwordTextInputLayout.getEditText().setError("Password must be more than 6 characters.");
+            confirmPasswordTextInputLayout.getEditText().setError("Password must be more than 6 characters.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean confirmEmail() {
+        if (mEmail.equals("") || !Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()) {
+            emailTextInputLayout.getEditText().setError("Email is invalid.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean confirmName() {
+       if (mName.equals("")) {
+           nameTextInputLayout.getEditText().setError("Name cannot be empty.");
+           return false;
+       }
+       return true;
+    }
+
+    private void showProgressBar() {
+
+    }
+
+    private void hideProgressBar() {
+    }
+}
