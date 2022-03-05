a86bb9171bc12900c8ec23859798e974db1a47a2
timothykirathe <timothy.kirathe@student.moringaschool.com>
2022-03-05 12:59:20 +0300

    Begun working on Firebase authentication.

--- app/src/main/java/com/moringaschool/moversapp/ui/SignupActivity.java
+++ app/src/main/java/com/moringaschool/moversapp/ui/SignupActivity.java
@@ -12,40 +12,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.moversapp.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.name) TextInputLayout name;
     @BindView(R.id.email) TextInputLayout email;
     @BindView(R.id.password) TextInputLayout password;
     @BindView(R.id.confirmpassword) TextInputLayout confirmPassword;
-    @BindView(R.id.signup) Button signup;
-    @BindView(R.id.loginClick) TextView loginView;
+    @BindView(R.id.signup) Button signUpButton;
+    @BindView(R.id.loginClick) TextView loginClickTextView;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private String mName;
+    private String mEmail;
+    private String mPassword;
+    private String mConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         ButterKnife.bind(this);
-        signup.setOnClickListener(this);
-        loginView.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+        loginClickTextView.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if(view == loginView){
+        if(view == loginClickTextView){
             Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             finish();
-
-    }else if (view == signup){
-            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+        }
+        if (view == signUpButton){
+            Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             finish();
-    }
+        }
 
     }
     }
